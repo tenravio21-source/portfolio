26751ce8743daa36e61824647f3cf832bcc1d8aa
hitachi-payments <hitachi-payments@gmail.com>
2026-02-03 13:38:44 +0530

    added shared preference

--- lib/src/components/widgets/app_locale_controller.dart
+++ lib/src/components/widgets/app_locale_controller.dart
@@ -2,13 +2,17 @@ import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../shared/app_shared_preferences.dart';
+
 class AppLocaleController extends AsyncNotifier<String> {
   @override
-  FutureOr<String> build() {
-    return Future.value('en');
+  FutureOr<String> build() async {
+    final savedLocale = await AppSharedPreferences.getLocale();
+    return savedLocale ?? 'en';
   }
 
-  void changeLocale(String locale) {
-    update((state) => locale);
+  void changeLocale(String locale) async {
+    AppSharedPreferences.setLocale(locale);
+    state = AsyncData(locale);
   }
 }
---
+++ lib/src/shared/app_shared_preferences.dart
@@ -0,0 +1,14 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppSharedPreferences {
+  static const String localeKey = 'app_locale';
+  static void setLocale(String locale) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(localeKey, locale);
+  }
+
+  static Future<String?> getLocale() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(localeKey) ?? 'en';
+  }
+}
